5855a8de5ad07aea5bf5477603728d858720e343
sushantao <sushantaojha97@gmail.com>
2019-09-10 14:52:32 +0545

    Basic blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-
 from .models import Post
 # Create your views here.
 
-posts= Post.objects.all()
-context={
-    'posts':posts,
-}
 def index(request):
-    # posts=get_object_or_404(BlogPost)
-    # context={
-    #     'posts':posts,
-    # }
+    posts=Post.objects.all()
+    context={
+        'posts':posts,
+    }
     return render(request, 'blog/index.html',context)
 
 def detail(request,posts_id):
-    
-    return render(request, 'blog/detail.html',context)
+    post = get_object_or_404(Post, pk= posts_id)
+    return render(request, 'blog/detail.html',{'post':post})
    
